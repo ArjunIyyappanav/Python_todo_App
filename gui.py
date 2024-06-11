ba26b8e6e142322d85bfb62fe9da51401830cba9
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-06-11 18:19:34 +0530

    GUI commit

---
+++ gui.py
@@ -0,0 +1,4 @@
+import FreeSimpleGUI as fsg
+window = fsg.Window('My to-do window',layout=[''])
+window.read()
+window.close() 
--- py1.py
+++ py1.py
@@ -1,3 +1,6 @@
-import webbrowser
-user_input = input("Enter Search item : ").replace(" ","+")
-webbrowser.open("https://google.com/search?q="+user_input)
+# import webbrowser
+# user_input = input("Enter Search item : ").replace(" ","+")
+# webbrowser.open("https://google.com/search?q="+user_input)
+import tkinter as tk
+root = tk.Tk()
+root.mainloop()
